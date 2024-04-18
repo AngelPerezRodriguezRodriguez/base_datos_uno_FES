39dca9d41235a52a6caa35b65a8572e0ca056a8f
Angel Pérez Rodríguez Rodríguez <angelperezrdguez@gmail.com>
2024-04-18 03:06:00 -0600

    feat: clase_09 parte dos

--- clase_09.sql
+++ clase_09.sql
@@ -1,8 +1,21 @@
 USE colegio;
 /*
+Producto cartesiano
+JOIN con comas (INNER JOIN)
+JOIN con palabra reservada (ON, USING y NATURAL)
+
+INNER JOIN
+
+RIGHT JOIN
+RIGHT JOIN (WHERE a.key IS NULL)
+
+LEFT JOIN
+LEFT JOIN (WHERE b.key IS NULL)
+
+
 El producto cartesiano es la multiplicación de todos los renglones de una tabla
 por todos los renglones de otra, y la suma de los campos de la primera y la segunda.
-No nos da información real pero es la base para la futura operación JOIN.
+No nos da información real, pero es la base para la futura operación JOIN.
 
 Para el producto cartesiano no importa el orden en que realicemos la multiplicación. 
 Pero como buena práctica hay que poner como primera tabla la que será el eje
@@ -10,7 +23,7 @@ de las relaciones. Nuestras operaciones o consultas, en algún momento se van a 
 degradando al trabajar con un gran número de renglones.
 
 La operación que debemos realizar para que se cumpla el modelo relacional es el JOIN.
-El JOIN es el producto cartesiano más la igualdad de la llave primaria y foránea.
+El JOIN es el producto cartesiano más la igualdad de la PK y la FK.
 Obtenemos información válida, ej. a qué pago pertenece qué alumno.
 */
 
@@ -71,7 +84,7 @@ cursos,
 grados, 
 niveles
 WHERE id_salon = 1;
--- Cada salón aparece 4 x 17 x 7 = 476 veces
+-- ... cada salón aparece 4 x 17 x 7 = 476 veces
 
 
 
@@ -82,11 +95,12 @@ cursos c
 WHERE s.id_curso = c.id_curso 
 ORDER BY 1;
 /*
- * Obtenemos 191 registros
+INNER JOIN.
+191 registros.
 
- * Para que el producto cartesiano se convierta en un JOIN igualamos PK y FK
- * El número máximo de registros de un JOIN depende de la tabla eje, pero pueden ser menos
- * También podemos renombrar las tablas
+Para que el producto cartesiano se convierta en un JOIN igualamos la PK y la FK.
+El número máximo de registros de un JOIN depende de la tabla eje, pero pueden ser menos.
+También podemos renombrar las tablas.
  */
 
 SELECT *
@@ -98,7 +112,8 @@ WHERE
 s.id_curso = c.id_curso AND
 s.id_grado = g.id_grado
 ORDER BY 1;
--- Obtenemos 191 registros
+-- INNER JOIN
+-- 191 registros
 
 SELECT *
 FROM
@@ -112,7 +127,8 @@ s.id_grado = g.id_grado AND
 s.id_nivel = n.id_nivel
 ORDER BY 1;
 /*
-Obtenemos 198 registros.
+INNER JOIN.
+Pasamos de 191 registros a 198.
 
 Existen salones que no tienen un nivel:
 
@@ -122,9 +138,9 @@ WHERE id_nivel = 0;
 Cero no existe en la tabla niveles, es decir,
 no es un id_nivel válido.
 
-Listamos los salones y a qué curso, grado y nivel pertenecen (sentencia actual INNER JOIN)
+Obtener los salones y a qué curso, grado y nivel pertenecen (sentencia actual INNER JOIN)
 no es lo mismo que
-listamos todos los salones (sentencia diferente).
+Obtener todos los salones (sentencia diferente LEFT JOIN).
 */
 
 SELECT *
@@ -136,6 +152,7 @@ id_nivel NOT IN(
     FROM niveles
 )
 ORDER BY 1;
+-- ... actúa como un LEFT JOIN (WHERE b.key IS NULL)
 -- ... obtenemos los registros faltantes
 
 SELECT *
@@ -145,6 +162,8 @@ WHERE
 id_nivel NOT IN(1, 2, 3, 4, 5, 6, 7)
 ORDER BY 1;
 -- ... misma sentencia que la anterior
+
+
 
 /*
 Las subconsultas pueden ocuparse en cualquier parte de una consulta SQL.
@@ -212,8 +231,8 @@ s.id_curso = c.id_curso AND
 s.id_grado = g.id_grado AND
 s.id_nivel = n.id_nivel
 ORDER BY 1;
--- Para que parezca un reporte hay que realizar una proyección adecuada,
--- pues tenemos muchos campos. Filtramos los campos que nos interesan por tabla
+-- Para que parezca un reporte hay que realizar una proyección adecuada
+-- Filtramos los campos que nos interesan de cada tabla
 
 
 
@@ -234,8 +253,10 @@ s.id_grado = g.id_grado AND
 s.id_nivel = n.id_nivel AND
 n.nombre = 'secundaria'
 ORDER BY 4;
--- El filtro debe de definirse por los datos que nos piden
--- Listamos los salones de secundaria
+/*
+INNER JOIN
+Obtenemos los salones de secundaria
+*/
 
 SELECT
 id_salon,
@@ -255,14 +276,15 @@ s.id_nivel = n.id_nivel AND
 n.nombre = 'secundaria' AND
 g.nombre = '3o'
 ORDER BY 4;
--- ... listamos los salones de tercero de secundaria
+-- ... obtenemos los salones de tercero de secundaria
 
 
 
 SELECT *
 FROM salones
 WHERE id_nivel IN(1, 2, 3, 4, 5, 6, 7);
--- Listamos los salones que tienen un nivel válido
+-- Actúa como un INNER JOIN
+-- Obtenemos los salones que tienen un nivel válido
 
 SELECT *
 FROM salones
@@ -275,7 +297,8 @@ WHERE id_nivel IN(
 SELECT *
 FROM salones
 WHERE id_nivel NOT IN(1, 2, 3, 4, 5, 6, 7);
--- Listamos los salones que no tienen un nivel válido
+-- Actúa como un LEFT JOIN (WHERE b.key IS NULL)
+-- Obtenemos los salones que no tienen un nivel válido
 
 SELECT *
 FROM salones
@@ -292,7 +315,7 @@ WHERE id_nivel IN(
     FROM niveles 
     WHERE id_nivel > 2
 );
--- Listamos los salones que tienen un nivel mayor que dos
+-- Obtenemos los salones que tienen un nivel mayor que dos
 
 
 
@@ -314,16 +337,18 @@ SELECT COUNT(*)
 FROM 
 pagos,
 alumnos;
--- 808 x 4642 = 3750736
--- Usamos COUNT porque son demasiados registros para el buffer
-
+/*
+Producto cartesiano.
+808 x 4642 = 3750736 registros.
+Usamos COUNT porque son demasiados registros para el buffer.
+*/
 
 SELECT * 
 FROM 
 pagos,
 alumnos 
 LIMIT 10;
--- Si es necesario ver la estructura, limitamos los renglones
+-- ... si es necesario ver la estructura, limitamos los renglones
 
 
 
@@ -334,7 +359,8 @@ alumnos a
 WHERE
 p.clave_alu = a.clave_alu
 ORDER BY 1;
--- Obtenemos 4642 registros
+-- INNER JOIN
+-- 4642 registros
 
 SELECT *
 FROM 
@@ -345,8 +371,11 @@ WHERE
 p.clave_alu = a.clave_alu AND
 p.id_curso = c.id_curso
 ORDER BY 1;
--- Obtenemos 4641 registros
--- Existe un pago que no tiene curso
+/*
+INNER JOIN.
+4641 registros.
+Existe un pago que no tiene curso.
+*/
 
 SELECT *
 FROM 
@@ -356,6 +385,7 @@ id_curso NOT IN (
 	SELECT id_curso 
     FROM cursos
 );
+-- ... actúa como un LEFT JOIN (WHERE b.key IS NULL)
 -- ... obtenemos el registro faltante
 
 
@@ -396,7 +426,8 @@ p.id_curso = c.id_curso AND
 p.pago < 2000 AND
 a.sexo = 'f'
 ORDER BY 1;
--- Listamos las mujeres con pagos menores a dos mil
+-- INNER JOIN
+-- Obtenemos las mujeres con pagos menores a dos mil
 
 SELECT
 a.clave_alu AS a_clave_alu,
@@ -414,6 +445,10 @@ WHERE
 p.clave_alu = a.clave_alu AND
 p.id_curso = c.id_curso
 ORDER BY 1;
+-- ... misma sentencia que la anterior
+
+
+
 /*
 Podemos hacer uso de consultas anidadas para que la consulta principal sea más ágil,
 es decir, conseguimos que el tiempo de ejecución sea menor.
@@ -431,3 +466,200 @@ Con consultas anidadas
    728 pagos x 283 alumnos x 4 cursos
  * Igualamos PK y FK
  */
+ 
+ 
+ 
+SELECT
+a.clave_alu AS a_clave_alu,
+a.ap_paterno AS a_ap_paterno,
+a.ap_materno AS a_ap_materno,
+a.nombre AS a_nombre,
+p.pago AS p_pago,
+p.fecha_pago AS p_fecha_pago,
+c.abreviatura AS c_abreviatura
+FROM
+pagos p 
+JOIN alumnos a ON(p.clave_alu = a.clave_alu)
+JOIN cursos c ON(p.id_curso = c.id_curso)
+ORDER BY 1;
+/* 
+INNER JOIN.
+4641 registros.
+
+Sentencia equivalente:
+
+SELECT * 
+FROM  
+alumnos a, 
+pagos p, 
+cursos c
+WHERE 
+p.clave_alu = a.clave_alu AND 
+p.id_curso = c.id_curso
+ORDER BY 1;
+*/
+
+SELECT *
+FROM
+pagos p 
+JOIN alumnos a USING(clave_alu)
+JOIN cursos c USING(id_curso)
+ORDER BY 1;
+/*
+... misma sentencia que la anterior.
+
+Utilizamos USING cuando la PK y la FK se llaman iguales.
+Debemos definir explícitamente el nombre de dicho campo.
+
+Hay una sola proyección de la PK, no se muestra la FK 
+pero se puede utilizar. Sin ON O USING realizaos un producto cartesiano.
+*/
+
+SELECT *
+FROM
+pagos p 
+NATURAL JOIN alumnos a
+JOIN cursos c USING(id_curso)
+ORDER BY 1;
+/*
+... misma sentencia que la anterior.
+
+Utilizamos NATURAL cuando la PK y la FK se llaman iguales.
+El propio manejador infiere cuales cuál es la PK y la FK.
+
+A diferencia de USING, sólo lo podemos utilizar una vez.
+Si en el ejemplo anterior utilizamos NATURAL en vez USING, el manejador 
+no sabe inferir cuál es la PK ni la FK. Por lo tanto, debemos remplazar 
+el segundo NATURAL ya sea con ON o USING.
+*/
+
+
+
+SELECT
+a.clave_alu AS a_clave_alu,
+a.ap_paterno AS a_ap_paterno,
+a.ap_materno AS a_ap_materno,
+a.nombre AS a_nombre,
+p.pago AS p_pago,
+p.fecha_pago AS p_fecha_pago,
+c.abreviatura AS c_abreviatura
+FROM
+pagos p 
+RIGHT JOIN alumnos a ON(p.clave_alu = a.clave_alu)
+LEFT JOIN cursos c ON(p.id_curso = c.id_curso)
+ORDER BY 1;
+/*
+RIGHT JOIN y LEFT JOIN.
+4968 registros.
+Obtenemos todos los alumnos aunque no tengan un pago.
+
+Pasamos de 4641 registros a 4968.
+Es decir, ahora también contemplamos a los alumnos que no tengan un pago.
+
+RIGHT muestra los registros de la relación derecha 
+aunque no tenga correspondencia con la relación izquierda.
+
+LEFT muestra los registros de la relación izquierda 
+aunque no tenga correspondencia con la relación derecha.
+*/
+
+SELECT
+a.clave_alu AS a_clave_alu,
+a.ap_paterno AS a_ap_paterno,
+a.ap_materno AS a_ap_materno,
+a.nombre AS a_nombre,
+p.pago AS p_pago,
+p.fecha_pago AS p_fecha_pago,
+c.abreviatura AS c_abreviatura
+FROM
+pagos p 
+RIGHT JOIN alumnos a ON(p.clave_alu = a.clave_alu)
+LEFT JOIN cursos c ON(p.id_curso = c.id_curso)
+WHERE p.clave_alu IS NULL
+ORDER BY 1;
+/*
+RIGHT JOIN (WHERE a.key IS NULL).
+326 registros.
+Obtenemos todos los alumnos que no tengan un pago.
+*/
+
+SELECT
+a.clave_alu AS a_clave_alu,
+a.ap_paterno AS a_ap_paterno,
+a.ap_materno AS a_ap_materno,
+a.nombre AS a_nombre,
+no_pagos,
+total_pagos,
+c.abreviatura AS c_abreviatura
+FROM(
+	SELECT 
+    clave_alu,
+    id_curso,
+    COUNT(pago) AS no_pagos,
+    SUM(pago) AS total_pagos
+    FROM pagos
+    GROUP BY clave_alu, id_curso
+) p 
+RIGHT JOIN alumnos a ON(p.clave_alu = a.clave_alu)
+LEFT JOIN cursos c ON(p.id_curso = c.id_curso)
+ORDER BY 1;
+/*
+LEFT JOIN.
+808 registros.
+Obtenemos el total y el número de pagos de todos los alumnos registrados.
+
+482 registros de la subconsulta de la tabla pagos.La proyección de dicha subconsulta 
+limita la proyección de la consulta principal respecto a la misma tabla.
+
+Si utilizamos subconsultas en un JOIN, la proyección de dichas subconsultas
+deben incluir el campo utilizado en clausula ON, USING o NATURAL.
+*/
+
+
+SELECT
+a.clave_alu AS a_clave_alu,
+a.ap_paterno AS a_ap_paterno,
+a.ap_materno AS a_ap_materno,
+a.nombre AS a_nombre,
+IFNULL(no_pagos, 0) AS no_pagos,
+IFNULL(total_pagos, 0) AS total_pagos,
+IFNULL(c.abreviatura, 'SIN CURSO') AS c_abreviatura
+FROM(
+	SELECT 
+    clave_alu,
+    id_curso,
+    COUNT(pago) AS no_pagos,
+    SUM(pago) AS total_pagos
+    FROM pagos
+    GROUP BY clave_alu, id_curso
+) p 
+RIGHT JOIN alumnos a ON(p.clave_alu = a.clave_alu)
+LEFT JOIN cursos c ON(p.id_curso = c.id_curso)
+ORDER BY 1;
+-- ... para que parezca un reporte hay que realizar una proyección adecuada
+-- ... remplazamos los valores NULL con la función IFNULL
+
+SELECT
+a.clave_alu,
+CONCAT_WS(
+	' ',
+	a.ap_paterno,
+	a.ap_materno,
+	a.nombre
+) AS alumno,
+IFNULL(no_pagos, 0) AS no_pagos,
+IFNULL(total_pagos, 0) AS total_pagos,
+IFNULL(c.abreviatura, 'SIN CURSO') AS c_abreviatura
+FROM(
+	SELECT 
+    clave_alu,
+    id_curso,
+    COUNT(pago) AS no_pagos,
+    SUM(pago) AS total_pagos
+    FROM pagos
+    GROUP BY clave_alu, id_curso
+) p 
+RIGHT JOIN alumnos a ON(p.clave_alu = a.clave_alu)
+LEFT JOIN cursos c ON(p.id_curso = c.id_curso)
+ORDER BY 1;
+-- ... concatenamos el nombre completo de los alumnos
